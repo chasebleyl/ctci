23e7d61ec05c53bf45269f3c6bbd306626d43449
Chase Bleyl <chase@bleyl.me>
2019-06-06 00:17:38 +0000

    Adding alternative solutions to exercise 2

--- data-structures/interview-questions/arrays_and_strings/1_2.py
+++ data-structures/interview-questions/arrays_and_strings/1_2.py
@@ -13,20 +13,46 @@ def validate_permutations(permutations, expected_result):
 			print("For strings " + str(permutation[0]) + " and " + str(permutation[1])  + " expected " + str(expected_result) + " but got " + str(result))
 
 ###
-# First Attempt (with help from https://github.com/careercup/CtCI-6th-Edition-Python/blob/master/Chapter1/2_Check%20Permutation/CheckPermutation.py)
+# Second Attempt (with help from solution): No external packages required
 # O(N)
-
 def check_permutation(string_one, string_two):
 	if len(string_one) != len(string_two):
 		return False
-	counter = Counter()
+	counter = [ 0 ] * 128 # ASCII assumption
 	for char in string_one:
-		counter[char] += 1
+		counter[ord(char)] += 1
 	for char in string_two:
-		if counter[char] == 0:
+		if counter[ord(char)] == 0:
 			return False
-		counter[char] -= 1
-	return True    
+		counter[ord(char)] -= 1
+	return True 
+	
+# ###
+# # First Attempt (with help from https://github.com/careercup/CtCI-6th-Edition-Python/blob/master/Chapter1/2_Check%20Permutation/CheckPermutation.py)
+# # O(N)
+# def check_permutation(string_one, string_two):
+# 	if len(string_one) != len(string_two):
+# 		return False
+# 	counter = Counter()
+# 	for char in string_one:
+# 		counter[char] += 1
+# 	for char in string_two:
+# 		if counter[char] == 0:
+# 			return False
+# 		counter[char] -= 1
+# 	return True    
+
+# ###
+# # Third Attempt (with help from solution): sort strings then compare, less efficient than first & third but very clear
+# # O(N log N)
+# def check_permutation(string_one, string_two):
+# 	if len(string_one) != len(string_two):
+# 		return False
+# 	sorted_one = ''.join(sorted(string_one))
+# 	sorted_two = ''.join(sorted(string_two))
+# 	if sorted_one != sorted_two:
+# 		return False
+# 	return True
 	
 validate_permutations(valid_permutations, True)
 validate_permutations(invalid_permutations, False)
